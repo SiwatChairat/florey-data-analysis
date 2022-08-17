973feed7341a02c5b5acd16c6d963dc578275f07
SiwatChairat <siwat.chairat@gmail.com>
2022-08-17 15:54:18 +1000

    data cleaning on medical history file

--- dataCleaning.py
+++ dataCleaning.py
@@ -2,40 +2,46 @@ import pandas as pd
 from pandas_streaming.df import StreamingDataFrame
 from pathlib import Path
 
-file = "testData/ADNIMERGE.csv"
 
-# unused headers 
-unuse_header = ["COLPROT","ORIGPROT","PTID","SITE","FLDSTRENG", "FSVERSION", "FSVERSION_bl", "FLDSTRENG_bl", "IMAGEUID", "IMAGEUID_bl"]
+adni_merge = "data/ADNIMERGE.csv"
+medhist = "data/RECMHIST.csv"
+
+
+
+# used headers 
+adni_use_header = ["RID", "VISCODE", "EXAMDATE", "DX_bl", "AGE", "PTGENDER", "PTEDUCAT", "PTETHCAT", "PTRACCAT", "PTMARRY",
+               "APOE4", "DX", "Month"]
+medh_use_header = ["RID", "EXAMDATE", "RECNO", "MHDESC"]
+
+
 
 def get_headers(file_name):
     headers = list(pd.read_csv(file_name,sep="|",nrows=1).columns)[0].replace('"',"").split(",")
-    print(headers)
-    print(len(headers))
     return headers
 
 
 
 # remove irrevant headers from the file and return a .csv file
-def clean_data(file_name ,unuse_header):
+def clean_data(file_name ,use_header):
     headers = get_headers(file_name)
     df = pd.read_csv(file_name)
     fname = Path(file_name).stem
     print(headers)
-    for i in range(len(unuse_header)):
-        header = unuse_header[i]
-        if header in headers:
-            print("Headers exist")
-            print(header)
-            df.pop(header)
+    for i in range(len(headers)):
+        if headers[i] not in use_header:
+            print("OK: popping header")
+            df.pop(headers[i])
         else:
             print("Error: Headers do not exist")
     df.to_csv("processed_" + fname + ".csv")
 
 # clean data by removing unused headers
-#print(unuse_header)
-#clean_data(file, unuse_header)
+#print(use_header)
 
+# clean ADNIMERGE file by removing all unuse headers
+#clean_data(adni_merge, adni_use_header)
 
-
+# clean RECMHIST file by removing all unuse headers
+#clean_data(medhist, medh_use_header)
 
 
--- main.py
+++ main.py
@@ -7,4 +7,22 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
+admi_merge  = "processed_ADNIMERGE.csv"
+medhist = "processed_RECMHIST.csv"
+df = pd.read_csv(file_name)
 
+
+# get data in column a given that b is equal to the desired data c 
+def get_patient_data(a, b, c):
+    res = df.loc[df[b] == c, a]
+    return res 
+
+# get distinct data in column a 
+def distinct_val(df, a):
+    res = df[a].dropna().unique()
+    return res
+
+viscode = distinct_val(df, "VISCODE")
+
+def to_json(df):
+    
